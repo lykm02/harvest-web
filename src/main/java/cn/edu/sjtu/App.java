9d6ce08d57be4a5faaa414ed47d4daeedf34abe6
kmchu <kmchu@ctrip.com>
2014-04-09 19:04:00 +0800

    all greeting exmaple

---
+++ src/main/java/cn/edu/sjtu/App.java
@@ -0,0 +1,12 @@
+package cn.edu.sjtu;
+
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan
+public class App {
+
+    public static void main(String[] args) {
+        //SpringApplication.run(Application.class, args);
+    }
+
+}
